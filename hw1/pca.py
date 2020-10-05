cb64c01c54b7a1635a547a285f2107dff74d0f9b
allen0307 <chinlun1999@gmail.com>
2020-10-06 00:58:57 +0800

    modify

--- hw1/pca.py
+++ hw1/pca.py
@@ -12,7 +12,6 @@ for person in range(40):
     for image in range(10):
         im = Image.open(picture_path + "/" + str(person + 1) + "_" + str(image + 1) + ".png")
         data = np.array(im)
-
         picture = [] #放每張picture的矩陣
         for row in range(56):
             for column in range(46):
@@ -22,24 +21,10 @@ for person in range(40):
         else:
             trainset.append(picture)
     
-
 trainset = np.array(trainset)
 testset = np.array(testset)
 
-print("row : ",np.size(trainset,0))
-print("column : ",np.size(trainset,1))
-# print("row : ",np.size(testset,0))
-# print("column : ",np.size(testset,1))
-
-sum_vector = [0] * 2576
-
-for row in range(360):
-    for entry in range(2576):
-        sum_vector[entry] += trainset[row][entry]
-
-for entry in range(2576):
-    sum_vector[entry] = int(sum_vector[entry] / 360)
-
+#輸入是一個拉成一條線(1*2576)的矩陣，輸出是將此矩陣轉成(56*46)的np.array
 def arraytoimage(array):
     eigenface_array = []
     for i in range(2576):
@@ -52,11 +37,25 @@ def arraytoimage(array):
             eigenface_array.append(row)
     return np.array(eigenface_array)
 
-eigenface = Image.fromarray(np.uint8(arraytoimage(sum_vector)))
-eigenface.show()
+#==================================================================
+# sum_vector = [0] * 2576
 
+# for row in range(360):
+#     for entry in range(2576):
+#         sum_vector[entry] += trainset[row][entry]
+# for entry in range(2576):
+#     sum_vector[entry] = int(sum_vector[entry] / 360)
+# eigenface = Image.fromarray(np.uint8(arraytoimage(sum_vector)))
+# eigenface.show()
+#==================================================================
 new_data = pca.fit_transform(trainset)
 eigenvector = pca.components_
-for i in range(len(eigenvector)):
-    eigenface = Image.fromarray(np.uint8(arraytoimage(eigenvector[i])))
-    eigenface.show()
+
+eigenface_1 = []
+answer = [0] * 2576
+for i in range(3):
+    answer += eigenvector[0] * new_data[i][0]
+print(answer)
+# for i in range(len(eigenvector)):
+#     eigenface = Image.fromarray(np.uint8(arraytoimage(eigenvector[i])))
+#     eigenface.show()
